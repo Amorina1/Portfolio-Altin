a622003c32ae64827619a78fdcaf6413d81a389f
Altin <emaili_ynd@example.com>
2026-09-07 20:00:24 -0300

    Update deploy workflow

--- server.js
+++ server.js
@@ -5,21 +5,18 @@ app.use(express.json());
 
 let todos = [];
 
-// GET endpoint për të marrë listën
 app.get('/todos', (req, res) => {
   res.json(todos);
 });
 
-// POST endpoint për të shtuar një todo
 app.post('/todos', (req, res) => {
   const todo = req.body;
   todos.push(todo);
   res.status(201).json(todo);
 });
 
-// Ky rresht e mban serverin gjallë
-app.listen(8080, () => {
-  console.log('Server running on port 8080');
+app.listen(5000 => {
+  console.log('Server running on port 5000
 });
 
 app.get('/', (req, res) => {
